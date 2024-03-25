d7a0c4af2803948f013dcd277b7ac4511be3db1e
petershaan12 <peterhiku12@gmail.com>
2024-03-25 17:19:33 +0700

    baru

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <p className="text-center text-gray-500 text-xs py-12">
+      &copy; 2024 DIK DEA. All rights reserved.
+    </p>
+  );
+};
+
+export default Footer;
